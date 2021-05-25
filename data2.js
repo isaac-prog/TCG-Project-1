e4f06c85e7423043d492c70d06c74e6c41484826
isaac-prog <panda_mm88@yahoo.com>
2021-05-25 01:04:39 +0000

    graqph and data

---
+++ data2.js
@@ -0,0 +1,52 @@
+async function loadData(url) {
+    const response = await axios.get(url);
+    console.log(response.data.Level1);
+    console.log("load1");
+    return response.data.Level1;
+}
+
+
+function transformData(rawData){
+    // accumulator array that will store the final
+    // results
+    let series = [];
+    for (let datnum of rawData) {
+        // recreate the data point in the format
+        // that apex chart expects
+        series.push({
+            'x': parseInt(datnum.year),
+            'value': parse(datnum.level_1),
+            'y': parseFloat(datnum.value)
+        })
+    }
+    console.log(series);
+    console.log("series1");
+    return series;
+}
+
+
+async function loadData2(url) {
+    const response = await axios.get(url);
+    console.log(response.data.Level2);    
+    console.log("load2");
+    return response.data.Level2;
+}
+
+function transformData2(rawData){
+    // accumulator array that will store the final
+    // results
+    let series = [];
+    for (let datnum of rawData) {
+        // recreate the data point in the format
+        // that apex chart expects
+        series.push({
+            'x': parseInt(datnum.year),
+            'value': parse(datnum.level_1),
+            'sex': parse(datnum.level_2),
+            'y': parseFloat(datnum.value)
+
+        })
+    }
+    console.log("series2");
+    return series;
+}
---
+++ script2.js
@@ -0,0 +1,66 @@
+const chartOptions = {
+    'chart': {
+        'type':'line',
+        'height':'100%'
+    },
+    'series':[],
+    'noData':{
+        'text':"Please wait, loading data"
+    }
+}
+
+const chart = new ApexCharts(document.querySelector('#chart'), chartOptions);
+chart.render();
+
+
+// global variable
+let data;
+let preFilter;
+
+// event handlers
+document.querySelector("#filter-btn").addEventListener('click', function(){
+  //Trying to change/update the chart to the 2nd Data with Sex
+  //updateTable("cTest.json");
+
+    let startYear = parseInt(document.querySelector('#start-year').value);
+    let endYear = parseInt(document.querySelector('#end-year').value);
+    //let inputSex = parse(document.querySelector('#sex').value);
+
+    let filtered = data.filter(function(d){
+        return d.x >= startYear && d.x <= endYear //&& d.sex == inputSex;
+    })
+    
+    chart.updateSeries([
+        {
+            'name': "Druggies",
+            'data': filtered
+        }
+    ])
+})
+
+// wait for all the DOM elements
+// to be created then load in the CSV file
+
+window.addEventListener('DOMContentLoaded', async function(){
+    data = await loadData('cTest.json');
+    data = transformData(data);
+
+    chart.updateSeries([
+        {
+            'name':'Druggies',
+            'data': data
+        }
+    ])
+})
+
+window.addEventListener('DOMContentLoaded',async function(){
+    preFilter = await loadData2('cTest.json');
+    preFilter = transformData2(preFilter);
+
+    chart.updateSeries([
+        {
+            'name':'Druggies',
+            'data': preFilter
+        }
+    ])
+})
