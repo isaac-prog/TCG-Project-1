aa1469e75667b3c3ca591334e0f0eafc0b053488
isaac-prog <panda_mm88@yahoo.com>
2021-05-27 06:51:25 +0000

    added news search bar and optimised search function

--- news.js
+++ news.js
@@ -1,15 +1,27 @@
+let newsCounter=0;
+
 async function loadNewsApi() {
-  let response = await axios.get('https://gnews.io/api/v4/search?q=cocaine&token=ab6e3bcb8285c9465972109f6f1a7761');
+  let apiKey = 'ab6e3bcb8285c9465972109f6f1a7761'
+  let newsSearch = document.querySelector('#news-search').value;
+  newsCounter += 1;
+  if (newsCounter>1){
+    document.getElementById('news-display').innerHTML = "";}
+  
+  let response = await axios.get('https://gnews.io/api/v4/search?q='+'"'+newsSearch.replace(/\s+/g, '-')+'"'+'&token='+apiKey);
   updateDisplay(response.data.articles);
   console.log(response.data.articles)
 };
 
-loadNewsApi();
+let btn = document.querySelector('#search-btn');
+
+btn.addEventListener('click', function(){
+  loadNewsApi();
+})
 
 function updateDisplay(articles) {
   for (let eachArticles of articles) {
     let htmlSnippet = `<div class="card" style="width: 100%;">
-            <div class="card-body">
+            <div class="card-body" id="news-body">
                 <h5 class="card-title">${eachArticles.title}
                 <div class="card-text">
                     <ul>
